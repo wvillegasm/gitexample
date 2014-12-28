b661a409b6839450a7521e32cc7af2d9695a44b2
Wilfredo Villegas <wivisisorg@gmail.com>
2014-12-28 13:44:59 -0600

    Adding routing with parameters

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var express = require('express'),
 	app = express(),
 	PORT = 3000;
 
+
 app.get('/', function(req, res) {
 	res.end('hello world!');
 });
+
+app.get('/name/:user_name', function(req, res) {
+	res.status(200);
+	res.set('Content-type', 'text/html');
+	res.send("<html><body><h1>Hello " + req.params.user_name + "</h1></body></html>");
+})
 
 app.listen(PORT, function() {
 	console.log('The server is running, please, ' +
